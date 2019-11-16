95be2385d653d7a0641d40ed73d2765c35e3001d
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2019-11-16 11:47:44 +0100

    Funcion flor

--- tortuga.py
+++ tortuga.py
@@ -2,9 +2,32 @@ import turtle
 tortu = turtle.Turtle ()
 
 tortu.shape ("turtle")
+tortu.speed(-1)
+
+def flor(tipo):
+    for nr in range (18):
+        if tipo == 'Cu':
+            cuadrado()
+        elif tipo == 'C':
+            tortu. circle (40)
+        elif tipo = 'T':
+            triangulo()
+        tortu. left (20)
+    tallo(200)
+ 
+
+def tallo():
+    tortu.right (90)
+    tortu.forward ()
 
 
-def cuadrado:
+def triangulo():
+    for nr in range (3):
+        tortu.forward (200)
+        tortu.left (120)
+
+
+def cuadrado():
     for nr in range (4):
         tortu.forward (200)
         tortu.left (90)
@@ -14,40 +37,25 @@ eleccion = input ()
 
 if eleccion == "Cu":
     tortu.color ("red")
-    for nr in range (18):
-        cuadrado()
-        tortu. left (20)
-    tortu.right (90)
-    tortu.forward (500)
     
 elif eleccion == "T":
     tortu.color ("green")
-    for nr in range (18):
-        for nr in range (3):
-            tortu.forward (200)
-            tortu.left (120)
-        tortu.left (20)
-    tortu.right (90)
-    tortu.forward (500)
-        
+    
 elif eleccion == "C":
     tortu.color ("blue")
-    for nr in range (18):
-        tortu. circle (40)
-        tortu.left (20)
-    tortu.right (90)
-    tortu.forward (500)
+
+
+flor(eleccion)
+
+
+
     
+
+
+'''    
 else:
     print ("Has escrito la respuesta incorrecta")
-    
-
-
-
-    
-
-
-
+'''
 
 
 
