6191ff9784502fefb3fd56f86ccd07f6e3d65544
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2019-11-16 12:26:00 +0100

    Con modulos

---
+++ flors.py
@@ -0,0 +1,27 @@
+def flor(tipo):
+    for nr in range (18):
+        if tipo == 'Cu':
+            cuadrado()
+        elif tipo == 'C':
+            tortu. circle (40)
+        elif tipo = 'T':
+            triangulo()
+        tortu. left (20)
+    tallo(200)
+ 
+
+def tallo():
+    tortu.right (90)
+    tortu.forward ()
+
+
+def triangulo():
+    for nr in range (3):
+        tortu.forward (200)
+        tortu.left (120)
+
+
+def cuadrado():
+    for nr in range (4):
+        tortu.forward (200)
+        tortu.left (90)
--- tortuga.py
+++ tortuga.py
@@ -1,39 +1,13 @@
 import turtle
+import flors
+
 tortu = turtle.Turtle ()
 
 tortu.shape ("turtle")
 tortu.speed(-1)
 
-def flor(tipo):
-    for nr in range (18):
-        if tipo == 'Cu':
-            cuadrado()
-        elif tipo == 'C':
-            tortu. circle (40)
-        elif tipo = 'T':
-            triangulo()
-        tortu. left (20)
-    tallo(200)
- 
 
-def tallo():
-    tortu.right (90)
-    tortu.forward ()
-
-
-def triangulo():
-    for nr in range (3):
-        tortu.forward (200)
-        tortu.left (120)
-
-
-def cuadrado():
-    for nr in range (4):
-        tortu.forward (200)
-        tortu.left (90)
-
-print ("¿Cómo quieres dibujar tu flor: con C (circulos), con T (triángulos) o con Cu (Cuadrados)?")
-eleccion = input ()
+eleccion = input("¿Cómo quieres dibujar tu flor: con C (circulos), con T (triángulos) o con Cu (Cuadrados)?")
 
 if eleccion == "Cu":
     tortu.color ("red")
